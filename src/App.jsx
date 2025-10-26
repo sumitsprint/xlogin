18cc52a706402ddb80c698952a873c2fbed6de40
sumitsprint <ncr2099@gmail.com>
2025-10-26 09:37:15 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from "react";
 
 function App() {
@@ -20,6 +21,80 @@ function App() {
       setMessage("Both fields are required.");
       return;
     }
+    if (userName === "user" && password === "password") {
+      setMessage("Welcome, user!");
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+
+      {message && (
+        <p role="alert">
+          {message}
+        </p>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="userName">Username</label>
+          <input
+            placeholder="username"
+            id="userName"
+            value={userName}
+            type="text"
+            name="userName"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            placeholder="password"
+            id="password"
+            value={password}
+            type="password"
+            name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+*/
+import { useState } from "react";
+
+function App() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "userName") {
+      setUserName(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // if (!userName.trim() || !password.trim()) {
+    //   setMessage("Both fields are required.");
+    //   return;
+    // }
     if (userName === "user" && password === "password") {
       setMessage("Welcome, user!");
     } else {
